f110a0689914f0285374b1476db54254076ddd8b
jbjiang <jbjiang@kland.com.cn>
2024-01-31 11:08:29 +0800

    获取数据

--- lib/movie/list.dart
+++ lib/movie/list.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
-Dio dio = new Dio();
-
-// 默认显示第一页数据
-int page = 1;
-// 每页显示的数据条数
-int pagesize = 10;
-//电影列表数据
-var mlist = [];
-// 总数据条数，实现分页效果的
-var total = 0;
-
 class MovieList extends StatefulWidget {
   //固定写法
   // MovieList({Key key}) : super(key: key);
@@ -28,6 +17,17 @@ class MovieList extends StatefulWidget {
 
 //有状态控件  就必须有状态管理类 来进行实现
 class MovieListState extends State<MovieList> {
+  Dio dio = new Dio();
+
+// 默认显示第一页数据
+  int page = 1;
+// 每页显示的数据条数
+  int pagesize = 10;
+//电影列表数据
+  var mlist = [];
+// 总数据条数，实现分页效果的
+  var total = 0;
+
   //控件在创建的时候 会执行 initState
   @override
   void initState() {
@@ -41,6 +41,7 @@ class MovieListState extends State<MovieList> {
   Widget build(BuildContext context) {
     // TODO: implement build
     // return Text('这是信息页面----' + widget.mt);
+
     return ListView.builder(
       itemCount: mlist.length,
       itemBuilder: (BuildContext ctx, int i) {
@@ -49,20 +50,20 @@ class MovieListState extends State<MovieList> {
       },
     );
   }
+
+  getMovieList() async {
+    try {
+      //发送get请求
+      var response = await dio.get('http://192.168.0.176:9300/api/platform');
+      //处理数据
+      print(response);
+
+      // //数据赋值 使用函数 SetState
+      setState(() {
+        mlist = response.data;
+      });
+    } catch (error) {
+      print('Error fetching data: $error');
+    }
+  }
 }
-
-getMovieList() async {
-  //发送get请求
-  var response = await dio.get('http://192.168.0.176:9300/api/platform');
-
-  //处理数据
-  print(response);
-
-  //数据赋值 使用函数 SetState
-  setState(() {
-    //通过dio 返回的数据 必须使用[]  来访问
-    mlist = response as List;
-  });
-}
-
-void setState(Null Function() param0) {}
